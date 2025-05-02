286d296b049a8eb8bacc18d0df0005c7f17ebada
Shubham <hittheshubham1810@gmail.com>
2025-05-02 12:26:19 +0530

    after fixing the status code

--- src/controller/venderController.js
+++ src/controller/venderController.js
@@ -358,7 +358,7 @@ venderController.delete("/delete/:id", async (req, res) => {
 venderController.post("/my-booking/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const bookingList = await Booking.find({ venderId: id, status:req?.body?.status});
+    const bookingList = await Booking.find({ venderId: id, bookingStaus:req?.body?.status});
     const updatedBookingList = await Promise.all(
       bookingList.map(async (v) => {
         let serviceDetails = null;
