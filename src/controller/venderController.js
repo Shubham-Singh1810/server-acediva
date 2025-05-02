5562951e52921faf426c2b13979fdf3ce6e864ed
Shubham <hittheshubham1810@gmail.com>
2025-05-02 13:04:57 +0530

    after adding the currect filter key

--- src/controller/venderController.js
+++ src/controller/venderController.js
@@ -359,7 +359,7 @@ venderController.delete("/delete/:id", async (req, res) => {
 venderController.post("/my-booking/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const bookingList = await Booking.find({ venderId: id, bookingStaus:req?.body?.bookingStaus});
+    const bookingList = await Booking.find({ venderId: id, bookingStatus:req?.body?.bookingStaus});
     const updatedBookingList = await Promise.all(
       bookingList.map(async (v) => {
         let serviceDetails = null;
